de5e545d0a2090adc0055945e257e7755513aaa7
sumbat15 <sumbat15@gmail.com>
2021-08-15 05:29:12 +0300

    deploy

---
+++ precache-manifest.9ebc10d0d2016f3c80cc2b3d06ee8acd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6c6f152ebd4bb52b1a8",
+    "url": "/waste-tracker/css/app.c69bf296.css"
+  },
+  {
+    "revision": "bfe8ae6bbb445833a77a",
+    "url": "/waste-tracker/css/chunk-vendors.cd7e3f26.css"
+  },
+  {
+    "revision": "a4f819ddd748147ecc1301d3e7de4613",
+    "url": "/waste-tracker/img/box.a4f819dd.svg"
+  },
+  {
+    "revision": "38bf504760093a8a767a4e8e13f2f5ba",
+    "url": "/waste-tracker/index.html"
+  },
+  {
+    "revision": "f6c6f152ebd4bb52b1a8",
+    "url": "/waste-tracker/js/app.a78a7941.js"
+  },
+  {
+    "revision": "bfe8ae6bbb445833a77a",
+    "url": "/waste-tracker/js/chunk-vendors.53de607e.js"
+  },
+  {
+    "revision": "482b4c45bfef2bcc0eb3e1b2c574d74a",
+    "url": "/waste-tracker/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/waste-tracker/robots.txt"
+  }
+]);
